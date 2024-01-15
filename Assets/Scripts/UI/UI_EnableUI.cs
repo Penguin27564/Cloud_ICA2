4cbc3a755659e74b35d47d57bfb6c09ad0917518
Penguin27564 <penguin27564@gmail.com>
2024-01-16 00:30:09 +0800

    Added required ui for friends

--- Assets/Scripts/UI/UI_EnableUI.cs
+++ Assets/Scripts/UI/UI_EnableUI.cs
@@ -6,20 +6,25 @@ public class UI_EnableUI : MonoBehaviour
     [SerializeField]
     private List<GameObject> _currentUI, _UIToEnable;
 
+    [SerializeField]
+    private bool _toggle = false;
+
     public void ChangeUI()
     {
         if (_currentUI.Count > 0)
         {
             foreach (var UI in _currentUI)
             {
-                UI.SetActive(false);
+                if (!_toggle) UI.SetActive(false);
+                else UI.SetActive(!UI.activeInHierarchy);
             }
         }
         if (_UIToEnable.Count > 0)
         {
             foreach (var UI in _UIToEnable)
             {
-                UI.SetActive(true);
+                if (!_toggle) UI.SetActive(true);
+                else UI.SetActive(!UI.activeInHierarchy);
             }
         }
     }
